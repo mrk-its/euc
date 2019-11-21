1d5b394b6a1eaf96a8486c5c83df7b1301642ebd
Mariusz Kryński <mrk@sed.pl>
2019-11-21 16:44:34 +0100

    add utility for spawning async tasks, bump revision

---
+++ euc/utils/__init__.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+_tasks = []
+
+
+def _cleanup_task_cb(self, task):
+    logger.debug("cleanup task %r", task)
+    self._tasks.remove(task)
+
+
+def create_task(coro, loop=None):
+    loop = loop or asyncio.get_event_loop()
+    task = loop.create_task(coro)
+    _tasks.append(task)
+    task.add_done_callback(_cleanup_task_cb)
+    logger.debug("created task %r", task)
